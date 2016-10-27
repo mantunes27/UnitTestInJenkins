431af61842b546aec4c8cc0801d5c2fa9013745c
mantunes27 <marcoantunes86@gmail.com>
2016-10-27 15:43:23 +0100

    project made in Visual Studio with unit tests

---
+++ GettingStarted/Calculator.Tests/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+
+
+        }
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Multiply(2, 10);
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void ShouldAddDouble()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.AddDoubles(1.1, 2.2);
+
+            Assert.That(result, Is.EqualTo(3.3).Within(0.01));
+        }
+
+        [Test]
+        public void ShouldNotBeBiggerThan100()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.AddDoubles(1.1, 2.2);
+
+            Assert.That(result, Is.Not.GreaterThan(100));
+        }
+
+        [Test]
+        public void ShouldJoinNames()
+        {
+            var sut = new NameJoiner();
+
+            var result = sut.Join("Sarah", "Smith");
+
+            Assert.That(result, Is.EqualTo("Sarah Smith"));
+        }
+
+        [Test]
+        public void ShouldJoinName_CaseInsentitiveAssertDemo()
+        {
+            var sut = new NameJoiner();
+
+            var fullName = sut.Join("sarah", "smith");
+
+            Assert.That(fullName, Is.EqualTo("SARAH SMITH").IgnoreCase);
+        }
+
+        [Test]
+        public void ShouldJoinNames_NotEqualDemo()
+        {
+            var sut = new NameJoiner();
+
+            var fullName = sut.Join("Sarah", "Smith");
+
+            Assert.That(fullName, Is.Not.EqualTo("Gentry Smith"));
+        }
+
+
+    }
+}
---
+++ GettingStarted/GettingStarted/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            // Bug for demo purposes
+            // return a + b;
+            return a * b; 
+        }
+
+        public double AddDoubles(double a, double b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int value, int by)
+        {
+            if (value > 100)
+            {
+                throw new ArgumentOutOfRangeException("by"); // bug demo
+            }
+
+            return value / by;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class NameJoiner
+    {
+        public string Join(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+    }
+
+ 
+
+    
+}
